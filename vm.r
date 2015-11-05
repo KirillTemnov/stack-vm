3f2a4fe4edd9a00a9939387b5e6195bb49e88bb8
KirillTemnov <allselead@gmail.com>
2015-11-06 02:36:53 +0300

    add stack mashine commands, refactor comments and word conversion

--- vm.r
+++ vm.r
@@ -3,7 +3,7 @@ REBOL [
     File: %vm.r
     Author: "Kirill Temnov"
     Date: 02/11/2015
-    Version: 0.1.0
+    Version: 0.1.5
     ]
 
 
@@ -30,6 +30,39 @@ REBOL [
 ; halt
 
 vitrual-mashine: make object! [
+
+    int-to-word: func [
+        "Convert integer number to a word (binary!)"
+        i [integer!]
+    ][
+        debase/base copy/part skip to-hex i 4 4 16
+    ]
+
+    word-to-int: func [
+        "Convert binary word to integer"
+        w [binary!]
+        /local b1 b2
+    ][
+        b1: pick w 1
+        b2: pick w 2
+        ; shift first byte to the left on 8 bits
+        b2 + shift/left b1 8
+    ]
+
+    swap-stack-values: func [
+        "Swap two top level stack values"
+        stack [block!]
+        /local a b
+    ][
+        a: pick stack 1
+        b: pick stack 2
+        remove/part stack 2
+        insert stack a
+        insert stack b
+        stack
+    ]
+
+
     with-one-arg-do: func [
       ; Execute fn on top level stack value,
       ; push result on stack and return stack
@@ -39,7 +72,7 @@ vitrual-mashine: make object! [
         val: fn (first stack)
         remove/part stack 1
         insert stack val
-        return stack
+        stack
     ]
 
     with-two-args-do: func [
@@ -52,18 +85,38 @@ vitrual-mashine: make object! [
         val: fn (first stack)  (second stack)
         remove/part stack 2
         insert stack val
-        return stack
+        stack
     ]
 
     stack: []
     code: #{}                           ; program code, loaded in vm
     registers: make object! [
         pc: 1                          ; program count
+        cf: 0                          ; carry flag
+        zf: 0                          ; zero flag
         ]
 
 
+    get-word: func [
+        "Get word (2 bytes) from binary"
+        from
+        index
+        /local first-byte second-byte
+    ][
+        if error?
+         try [
+            first-byte: to-binary to-char pick from index
+            index: index + 1
+            second-byte: to-binary to-char pick from index
+        ][
+            print "Error fetching word"
+            return #{0000}              ;
+        ]
+        join first-byte second-byte     ; big endian
+    ]
+
     reset: func [
-        { "reboot"  mashine }           ;
+        "'Reboot' mashine"
     ] [
         print "reset mashine"
         clear stack
@@ -71,14 +124,49 @@ vitrual-mashine: make object! [
     ]
 
     dump-state: func [
-        ;; dump mashine status
+        "Dump mashine status"
     ] [
         print ["STACK: " probe stack]
         print ["Regs: "  "PC: " registers/pc]
     ]
 
-    inc: func [x] [x + 1]
-    dec: func [x] [x - 1]
+    ;; vm instructions
+    inc: func [
+        "Increment byte value"
+        x [binary!]
+    ] [
+        int-to-word  1 + word-to-int x
+    ]
+
+    dec: func [
+        "Decrement byte value"
+        x [binary!]
+    ] [
+        int-to-word  -1 + word-to-int x
+    ]
+
+    add: func [
+        "Add one operand to another"
+        first-op [binary!]
+        second-op [binary!]
+        /local i1 i2
+    ] [
+        i1: word-to-int first-op
+        i2: word-to-int second-op
+        int-to-word i1 + i2
+    ]
+
+    sub: func [
+        "Substract one operand from another"
+        first-op  [binary!]
+        second-op [binary!]
+        /local i1 i2
+    ] [
+        i1: word-to-int first-op
+        i2: word-to-int second-op
+        int-to-word i1 - i2
+    ]
+
 
     one-byte-instructions: #{00 02 03 04 05 06 07 08 09 0A 98 99} ;
     two-byte-instructions: #{01}                                  ;
@@ -87,7 +175,10 @@ vitrual-mashine: make object! [
         "Get size of instruction in bytes"
         instruction
     ][
-        if none <> find two-byte-instructions instruction  [return 2]
+        if none <> find two-byte-instructions instruction  [
+            ; one byte - command, 2 bytes - data
+            return 3
+        ]
         if none <> find one-byte-instructions instruction  [return 1]
         return 1                        ; unknown instruction size: 1 byte
     ]
@@ -99,7 +190,7 @@ vitrual-mashine: make object! [
         /local op size arg offset
     ] [
         if error?
-         try [op: to binary! to char! pick code registers/pc]
+         try [op: to-binary to-char pick code registers/pc]
         [
             print "Reach end of code block."
             return false
@@ -108,7 +199,7 @@ vitrual-mashine: make object! [
         arg: none
         if size > 1 [
             offset: registers/pc + 1
-            arg: pick code offset       ; todo transform to byte
+            arg: get-word code offset
             ]
         registers/pc: registers/pc + size
         switch/default op [
@@ -119,22 +210,22 @@ vitrual-mashine: make object! [
             #{01} [insert stack arg]
 
             ; add
-            #{02}  [with-two-args-do stack :+]
+            #{02}  [with-two-args-do stack :add]
 
             ; sub
-            #{03}  [with-two-args-do stack :-]
+            #{03}  [with-two-args-do stack :sub]
 
             ; mul
-            #{04} [with-two-args-do stack :*]
-
-            ; neg
-            #{05} [with-one-arg-do stack :negate]
+            ;#{04} [with-two-args-do stack :*]
 
             ; and
-            #{06} [with-two-args-do stack :and]
+            #{05} [with-one-arg-do stack :and]
 
             ; or
-            #{07} [with-two-args-do stack :or]
+            #{06} [with-two-args-do stack :or]
+
+            ; xor
+            #{07} [with-two-args-do stack :xor]
 
             ; inc
             #{08} [with-one-arg-do stack :inc]
@@ -142,8 +233,17 @@ vitrual-mashine: make object! [
             ; dec
             #{09} [with-one-arg-do stack :dec]
 
-            ; pop
-            #{0A} [stack: next stack]
+            ; pop/drop
+            #{0A} [remove stack]
+
+            ; dup
+            #{0B} [insert stack pick stack 1]
+
+            ; over
+            #{0C} [insert stack pick stack 2]
+
+            ; swap
+            #{0D} [swap-stack-values stack]
 
             ; stat
             #{98} [dump-state]
