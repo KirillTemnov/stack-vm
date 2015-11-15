5a38a3c6a91c0a37a0b052e0bd256ecb710cb5df
KirillTemnov <allselead@gmail.com>
2015-11-15 15:10:36 +0300

    Create new file for testing vm

---
+++ tests/vm-spec.r
@@ -0,0 +1,11 @@
+REBOL [
+    Title: "Tests of simple stack vitrual mashine"
+    File: %vm-spec.r
+    Author: "Kirill Temnov"
+    Date: 03/11/2015
+    ]
+
+do %spec.r
+do %../vm.r
+
+vm: make vitrual-mashine []
